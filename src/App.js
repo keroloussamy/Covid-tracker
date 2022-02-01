afb492cc7a450edb2c740e432b4d12631c806f72
kerolous samy <keroloussamy98@gmail.com>
2022-02-02 01:43:07 +0200

    dispatch continents

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import './App.css';
+import Continents from './components/Continents/Continents';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import continentsThunk from './redux/thunk/continentsThunk';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(continentsThunk());
+  }, []);
+
   return (
     <div>
-      Hello
+      <Navbar />
+      <Header />
+      <Continents />
     </div>
   );
 }

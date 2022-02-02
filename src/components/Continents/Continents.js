bbc466433a11a3e9c2e2221cf587fb9ed95a0d63
kerolous samy <keroloussamy98@gmail.com>
2022-02-02 20:45:09 +0200

    Move dispatch to Continents

--- src/components/Continents/Continents.js
+++ src/components/Continents/Continents.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { continentsThunk } from '../../redux/thunk/continentsThunk';
 import Continent from '../Continent/Continent';
 
 const Continents = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(continentsThunk());
+  }, []);
+
   const continents = useSelector((state) => state.continentsReducer);
 
   return (

96264d7128e3cfce0ca400c5f05937067127437a
kerolous samy <keroloussamy98@gmail.com>
2022-02-02 20:36:49 +0200

    Add getContinentByNameAPI fun

--- src/api/continentsAPI.js
+++ src/api/continentsAPI.js
@@ -1,7 +1,11 @@
-const getContinentsAPI = async () => {
+export const getContinentsAPI = async () => {
   const data = await fetch('https://corona.lmao.ninja/v2/continents')
     .then((responce) => responce.json());
   return data;
 };
 
-export default getContinentsAPI;
+export const getContinentByNameAPI = async (continentName) => {
+  const data = await fetch(`https://corona.lmao.ninja/v2/continents/${continentName}`)
+    .then((responce) => responce.json());
+  return data;
+};

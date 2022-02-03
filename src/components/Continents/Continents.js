38207b0f12a3e242cc140e58ce9ddc8b489d048d
kerolous samy <keroloussamy98@gmail.com>
2022-02-03 04:36:05 +0200

    Add Search feature

--- src/components/Continents/Continents.js
+++ src/components/Continents/Continents.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { continentsThunk } from '../../redux/thunk/continentsThunk';
 import Continent from '../Continent/Continent';
+import classes from './Continents.module.css';
 
 const Continents = () => {
+  const [search, setSearch] = useState([]);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(continentsThunk());
@@ -11,12 +13,28 @@ const Continents = () => {
 
   const continents = useSelector((state) => state.continentsReducer);
 
+  useEffect(() => {
+    setSearch(continents);
+  }, [continents]);
+
+  const SearchHandler = (e) => {
+    setSearch(
+      continents.filter((c) => c.continent.toLowerCase().includes(e.target.value)),
+    );
+  };
+
   return (
-    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-      {continents?.map((continent) => (
-        <Continent key={continent.continent} continentData={continent} />
-      ))}
-    </div>
+    <>
+      <div className={classes.breaker}>
+        <p>CASES BY CONTINENTS</p>
+        <input type="text" placeholder="Search" onChange={SearchHandler} />
+      </div>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {search?.map((continent) => (
+          <Continent key={continent.continent} continentData={continent} />
+        ))}
+      </div>
+    </>
   );
 };
 
